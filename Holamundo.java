73ffb22a6d54d7921f8a8015d4314cc470170d32
thedamnyi <ayso.yireh0808@gmail.com>
2025-03-10 15:43:29 -0600

    Add files via upload

---
+++ Holamundo.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class Holamundo {
+    public static void main(String[] args) {
+        String mensaje = "¡Hola Mundo!";
+        JOptionPane.showMessageDialog(null, mensaje, "Saludos", JOptionPane.INFORMATION_MESSAGE);
+
+
+    }
+}
